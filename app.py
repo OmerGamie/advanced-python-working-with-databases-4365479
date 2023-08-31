ecf0c64c118f2ee91b0bcc78297a0dd124df3f36
Omer-Gamie <57861096+OmerGamie@users.noreply.github.com>
2023-08-31 19:50:17 +0000

    Filtering movies by release year

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import sqlite3
 connection = sqlite3.connect('movies.db')
 cursor = connection.cursor()
 
+# Filtering by release year
 
+release_year = (1985,)
+
+cursor.execute('SELECT * FROM movies WHERE year=?', release_year)
+
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
