d7096da8af9a5343910e3639d4b1cc7298f4b310
Omer-Gamie <57861096+OmerGamie@users.noreply.github.com>
2023-09-02 22:16:16 +0200

    Built a model with SQLAlchemy ORM

--- mysql-sqlalchemy-workspace/database.py
+++ mysql-sqlalchemy-workspace/database.py
@@ -1,5 +1,28 @@
 from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import registry
 
+# Create an engine to connect to MySQL Database
+# echo=True will turn on the logging of SQL queries, which can be useful for debugging
 engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/projects',
 	echo=True)
+
+# Create a registry to kepp track of classes 
+mapper_registry = registry()
+#mapper_registry.metadata
+
+# Generate the base class for mapped classes from the registry
+Base = mapper_registry.generate_base()
+
+# Define a mapped class called 'Project'
+class Project(Base):
+  __tablename__ = 'projects'
+  
+  # Define columns for the table 'projects'
+  Project_id = Column(Integer, primary_key=True)
+  title = Column(String(50))
+  description = Column(String(50))
+  
+  # Define the string representation of instances of the class
+  def __repr__(self):
+    return "<Project(title={0}, description={1})>".format(
+      self.title, self.description)
