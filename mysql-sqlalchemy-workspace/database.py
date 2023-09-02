8da49f770ce30e456dd7c8c6e256953398a31852
Omer-Gamie <57861096+OmerGamie@users.noreply.github.com>
2023-09-02 22:38:15 +0200

    Used SQLAlchemy to transact on a MySQL database

--- mysql-sqlalchemy-workspace/database.py
+++ mysql-sqlalchemy-workspace/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
-from sqlalchemy.orm import registry, relationship
+from sqlalchemy.orm import registry, relationship, Session
 
-engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/projects',
+engine = create_engine('mysql+mysqlconnector://root:new_password@localhost:3306/projects',
 	echo=True)
 
 mapper_registry = registry()
@@ -32,9 +32,27 @@ class Task(Base):
 
 Base.metadata.create_all(engine)
 
-
-
-
+with Session(engine) as session:
+  organize_closet_project = Project(title='Organize closet', 
+          description='Organize the closet by color and style')
+  
+  session.add(organize_closet_project)
+  
+  session.flush()
+  
+  tasks = [
+    Task(project_id=organize_closet_project.project_id,
+         description='Decide which clothes to donate'),
+    Task(project_id=organize_closet_project.project_id,
+         description='Organize summer clothes'),
+    Task(project_id=organize_closet_project.project_id,
+         description='Organize winter clothes')
+	]
+  
+  session.bulk_save_objects(tasks)
+  
+  session.commit()
+  
 
 
 
